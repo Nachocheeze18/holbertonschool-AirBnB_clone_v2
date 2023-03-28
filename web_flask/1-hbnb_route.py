75dc68473b861f25a77cc3c3f90828094c58e7ca
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-27 18:32:34 -0700

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Flask With Python'''
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hellohnbn():
+    name = request.args.get("name", "Hello HBNB!")
+    return name
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+    name = request.args.get("name", "HBNB")
+    return name
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
